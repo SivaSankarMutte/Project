06e243eb3657ebcf1aaae6efa9214d6565b042e7
SivaSankarMutte <84972317+SivaSankarMutte@users.noreply.github.com>
2021-07-31 13:28:55 +0530

    Update settings.py

--- GreenWynk/GreenWynk/settings.py
+++ GreenWynk/GreenWynk/settings.py
@@ -156,7 +156,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='sivasankarmutte@gmail.com'
-EMAIL_HOST_PASSWORD="K@lmno@123"
+EMAIL_HOST_PASSWORD=""
 
 django_heroku.settings(locals())
 
