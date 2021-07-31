a3eec17044258fb3679c41e580e1195b66034b5f
SivaSankarMutte <84972317+SivaSankarMutte@users.noreply.github.com>
2021-07-31 13:30:08 +0530

    Update settings.py

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -141,6 +141,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='sivasankarmutte@gmail.com'
-EMAIL_HOST_PASSWORD="K@lmno@123"
+EMAIL_HOST_PASSWORD=""
 
 django_heroku.settings(locals())
